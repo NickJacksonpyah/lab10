06a387054b4c1c5787f2cb6cc738fa7ae6eb1eea
NickJacksonpyah <s137665@gmail.com>
2014-10-21 14:17:25 -0500

    80/90 pt

--- 70-100pt.py
+++ 70-100pt.py
@@ -23,8 +23,12 @@ drawpad = Canvas(root, width=800,height=600, background='white')
 drawpad.grid(row=0, column=1)
 
 # Insert your code here to draw the house!
-
+# square
 square = drawpad.create_rectangle(200,200,300,300)
+# line
 line = drawpad.create_line(300, 200, 250, 175)
 line1 = drawpad.create_line(200, 200, 250, 175)
+line2 = drawpad.create_line(230, 300, 230, 270)
+square = drawpad.create_rectangle(230, 300, 250, 270)
+square1 = drawpad.create_rectangle(270, 185, 250, 160)
 root.mainloop()
